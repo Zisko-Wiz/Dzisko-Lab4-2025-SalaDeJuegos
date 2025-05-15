9f3147b6cc8a461933e26735d6f3dbbf99c39d3b
Zisko-Wiz <zisko_wiz@proton.me>
2025-05-15 21:28:47 +0000

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://ummakupfxqwffbwnrjud.supabase.co',
-  publicAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtbWFrdXBmeHF3ZmZid25yanVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MjU5MzcsImV4cCI6MjA2MjQwMTkzN30.JGcaDqxYG7u31ATjoySXLLMK1PA3BJTScXzQ2ndBCog'
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://ummakupfxqwffbwnrjud.supabase.co',
-  publicAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtbWFrdXBmeHF3ZmZid25yanVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MjU5MzcsImV4cCI6MjA2MjQwMTkzN30.JGcaDqxYG7u31ATjoySXLLMK1PA3BJTScXzQ2ndBCog'
-};
-
